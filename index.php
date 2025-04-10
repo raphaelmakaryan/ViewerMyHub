b592213722eb159a44460e08a4f6ec03d61ba74b
MAKARYAN Raphael <contact@raphaelmakaryan.fr>
2025-04-10 18:53:37 +0200

    🐛 Adding space between image and title

--- index.php
+++ index.php
@@ -142,7 +142,7 @@ require_once "./utils/timeFunction.php";
                                                                         $img = '<svg height="16" class="octicon octicon-git-merge close" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path d="M5.45 5.154A4.25 4.25 0 0 0 9.25 7.5h1.378a2.251 2.251 0 1 1 0 1.5H9.25A5.734 5.734 0 0 1 5 7.123v3.505a2.25 2.25 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.95-.218ZM4.25 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm8.5-4.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM5 3.25a.75.75 0 1 0 0 .005V3.25Z"></path></svg>';
                                                                     }
                                                                     ?>
-                                                                    <p class="fs-6 text-start text-truncate"><?php echo $img; ?><?php echo $issue['title']; ?></p>
+                                                                    <p class="fs-6 text-start text-truncate"><?php echo $img; ?> <?php echo $issue['title']; ?></p>
                                                                 </a>
                                                             </div>
                                                             <div class="col-2 ">
